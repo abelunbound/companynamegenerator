92a6124085734db4b78031d41a37e885d7616f00
Abel Akeni <5akena74@solent.ac.uk>
2022-04-17 16:43:09 +0100

    Company name generator

---
+++ namegenerator.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+
+print("Welcome to the Company Name Generator! Please, press enter to start!")
+
+#2. Ask the user for the city that they grew up in.
+
+word1 = input("What one word best describes your business idea? \n")
+
+#3. Ask the user for the name of a pet.
+word2 = input("What other word best decribes your business idea? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+
+print (word1+ word2 + " " + "Nigeria Limited")
+
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+
+#   https://replit.com/@appbrewery/band-name-generator-end
